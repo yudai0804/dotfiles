740d1cd74ae557c39a3f5465d057a5de895d38a1
yudai <yudai.yy0804@gmail.com>
2023-09-10 22:45:56 +0900

    add .tmux.conf

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
