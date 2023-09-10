d3a475d3a3329c880feb92e356ed0ef901b36b5b
yudai <yudai.yy0804@gmail.com>
2023-09-10 22:09:35 +0900

    delete zsh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
