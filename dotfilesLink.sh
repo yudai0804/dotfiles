27c050fd81a1ba1a0881ef3da3d1602a5398c5ed
yudai <yudai.yy0804@gmail.com>
2023-10-18 13:11:08 +0900

    add i3 config

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+mkdir -p .vim
+mkdir -p .config/ranger
+mkdir -p .config/alacritty
+mkdir -p .config/i3
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
@@ -7,3 +11,4 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
 ln -sf ~/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/dotfiles/i3/config ~/.config/i3/config
