888fadf000aa79743a2c6d05dc3baf447eeb86f9
yudai <yudai.yy0804@gmail.com>
2023-10-27 23:06:40 +0900

    enable history

--- .zshrc
+++ .zshrc
@@ -3,4 +3,5 @@ export LANG=ja_JP.UTF-8
 autoload -Uz compinit && compinit
 autoload -Uz colors && colors
 autoload -Uz add-zsh-hook
-source ~/dotfiles/zsh/prompt.zsh
+source ${HOME}/dotfiles/zsh/prompt.zsh
+source ${HOME}/dotfiles/zsh/history.zsh
---
+++ zsh/history.zsh
@@ -0,0 +1,9 @@
+export HISTFILE=$HOME/.zsh-history
+export HISTSIZE=10000
+export SAVEHIST=100000
+
+setopt hist_expire_dups_first # 履歴を切り詰める際に、重複する最も古いイベントから消す
+setopt hist_ignore_all_dups   # 履歴が重複した場合に古い履歴を削除する
+setopt hist_ignore_dups       # 前回のイベントと重複する場合、履歴に保存しない
+setopt hist_save_no_dups      # 履歴ファイルに書き出す際、新しいコマンドと重複する古いコマンドは切り捨てる
+setopt share_history          # 全てのセッションで履歴を共有する
