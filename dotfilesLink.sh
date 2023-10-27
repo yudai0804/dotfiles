3d5f326a05bece53c42e62e3a87a24f5e812797d
yudai <yudai.yy0804@gmail.com>
2023-10-27 21:27:16 +0900

    update dotfileslink

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-mkdir -p .config/ranger
-mkdir -p .config/alacritty
-mkdir -p .config/i3
+mkdir -p ~/.config/ranger
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/i3
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.xprofile ~/.xprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
-ln -sf ~/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
-ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf ~/dotfiles/i3/config ~/.config/i3/config
+ln -sf ~/dotfiles/.config/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -sf ~/dotfiles/.config/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sf ~/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/dotfiles/.confg/i3/config ~/.config/i3/config
