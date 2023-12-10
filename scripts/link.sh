b52a7dfc2b0bc8f3c6bc9f537683c4d08c6fb51e
yudai <yudai.yy0804@gmail.com>
2023-12-11 01:15:06 +0900

    fix: シンボリックリンクが循環参照になっていたので修正

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ln -sf ~/dotfiles/.config/i3 ~/.config/i3
-ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
-ln -sf ~/dotfiles/.config/ranger ~/.config/ranger
-ln -sf ~/dotfiles/.config/rofi ~/.config/rofi
-ln -sf ~/dotfiles/.config/wezterm ~/.config/wezterm
+ln -sfn ~/dotfiles/.config/i3 ~/.config/i3
+ln -sfn ~/dotfiles/.config/nvim ~/.config/nvim
+ln -sfn ~/dotfiles/.config/ranger ~/.config/ranger
+ln -sfn ~/dotfiles/.config/rofi ~/.config/rofi
+ln -sfn ~/dotfiles/.config/wezterm ~/.config/wezterm
 
 ln -sf ~/dotfiles/.dmrc ~/.dmrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
@@ -11,4 +11,3 @@ ln -sf ~/dotfiles/.xprofile ~/.xprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
 ln -sf ~/dotfiles/Wallpapers/bigsur.jpg ~/Pictures/Wallpapers/bigsur.jpg
-
