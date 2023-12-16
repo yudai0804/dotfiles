d1ab8d924f264186489f1009ba00320d71ea1ed8
yudai <yudai.yy0804@gmail.com>
2023-12-17 01:59:38 +0900

    update

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -157,6 +157,26 @@ return {
     end,
   },
 
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignore = false,
+        },
+        hide_by_name = {
+          ".DS_Sore",
+          "thumbs.db",
+        },
+        never_show = {},
+      },
+    },
+  },
+
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
