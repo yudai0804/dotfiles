bfd923f498871a6547821ba925f43d33f1288389
yudai <yudai.yy0804@gmail.com>
2024-08-01 16:56:47 +0900

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -67,7 +67,7 @@ __  __          __      _ ____  ____  ____  __ __ _
     fi
 
     # rust
-    if [ ! -f $HOME/.cargo/bin/cargo ]; then
+    if [ -n $(which cargo) ]; then
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     fi
 
