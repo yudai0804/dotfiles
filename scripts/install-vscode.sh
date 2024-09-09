2a508e90b9e3da56f68f8c3cd4e07ba67385ad91
yudai <yudai.yy0804@gmail.com>
2024-09-09 10:49:54 +0900

    update

---
+++ scripts/install-vscode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+# vscode
+# 参考:https://code.visualstudio.com/docs/setup/linux
+sudo apt-get install -y wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt install -y apt-transport-https
+sudo apt update
+sudo apt install -y code # or code-insiders
+# extensionsをインストール
+# ただし、ディレクトリの関係上ユーザーアカウントのときのみスクリプトが走るようにする
+if [ $(whoami) != root ]; then
+    ./install-vscode-extensions.sh
+fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -81,25 +81,7 @@ __  __          __      _ ____  ____  ____  __ __ _
         sudo apt install -y octave octave-signal
     fi
 
-    # 便利ツール
-
-    # vscode
-    # 参考:https://code.visualstudio.com/docs/setup/linux
-    if [ -z "$(which code)" ]; then
-        sudo apt-get install -y wget gpg
-        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-        sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-        echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-        rm -f packages.microsoft.gpg
-        sudo apt install -y apt-transport-https
-        sudo apt update
-        sudo apt install -y code # or code-insiders
-        # extensionsをインストール
-        # ただし、ディレクトリの関係上ユーザーアカウントのときのみスクリプトが走るようにする
-        if [ $(whoami) != root ]; then
-            ./install-vscode-extensions.sh
-        fi
-    fi
+    # tool
 
     # vim関連
     # もともと入っているvimを消してvim-gtkを入れる
