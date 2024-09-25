f84a9ff8969048318d067706236c6fdb1abacc1f
yudai <yudai.yy0804@gmail.com>
2024-09-25 12:04:21 +0900

    update

--- scripts/install-vscode-extensions.sh
+++ scripts/install-vscode-extensions.sh
@@ -7,5 +7,7 @@ code --install-extension cschlosser.doxdocgen
 code --install-extension ms-python.python
 code --install-extension ms-toolsai.jupyter
 code --install-extension ms-python.black-formatter
+# Rust
+code --install-extension rust-lang.rust-analyzer
 # Git/GitHub
 code --install-extension mhutchie.git-graph
